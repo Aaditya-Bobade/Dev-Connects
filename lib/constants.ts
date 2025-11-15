df6526a5a0c448d6134106fcab7cc775fa31f112
Aaditya Bobade <aadityabobade17@gmail.com>
2025-11-15 15:55:20 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem = {
+  image: string;
+  title: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+};
+
+export const events: EventItem[] = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1",
+    slug: "event-1",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event2.png",
+    title: "Event 2",
+    slug: "event-2",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event3.png",
+    title: "Event 3",
+    slug: "event-3",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event4.png",
+    title: "Event 4",
+    slug: "event-4",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event5.png",
+    title: "Event 5",
+    slug: "event-5",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event6.png",
+    title: "Event 6",
+    slug: "event-6",
+    location: "New York",
+    date: "2024-09-15",
+    time: "10:00 AM",
+  },
+]
